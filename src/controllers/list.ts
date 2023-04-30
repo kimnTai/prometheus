03ac694b6b0103dcba42e03b0e3b0847522fdcb1
kimn <a9523023650@gmail.com>
2023-04-30 20:48:02 +0800

    [ feat ] 列表 api 製作

---
+++ src/controllers/list.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import ListModel from "@/models/list";
+import CardModel from "@/models/card";
+
+export const createList = async (req: Request, res: Response) => {
+  const { name, boardId, position } = req.body;
+
+  const result = await ListModel.create({
+    name,
+    boardId,
+    position,
+  });
+
+  res.send({ status: "success", result });
+};
+
+export const updateList = async (req: Request, res: Response) => {
+  const { name, boardId, position, closed } = req.body;
+
+  const result = await ListModel.findOneAndUpdate(
+    { _id: req.params.listId },
+    { name, boardId, position, closed },
+    { new: true, runValidators: true }
+  );
+
+  if (!result) {
+    throw new Error("無此貼文 id");
+  }
+
+  res.send({ status: "success", result });
+};
+
+export const deleteList = async (req: Request, res: Response) => {
+  const result = await ListModel.findByIdAndDelete(req.params.listId);
+
+  const cardResult = await CardModel.updateMany(
+    {
+      listId: req.params.listId,
+    },
+    {
+      listId: "",
+      closed: true,
+    }
+  );
+
+  if (!result) {
+    throw new Error("無此貼文 id");
+  }
+
+  res.send({ status: "success", result: { result, cardResult } });
+};
+
+export const archiveAllCards = async (req: Request, res: Response) => {
+  const result = await CardModel.updateMany(
+    {
+      listId: req.params.listId,
+    },
+    {
+      listId: "",
+      closed: true,
+    }
+  );
+
+  res.send({ status: "success", result });
+};
