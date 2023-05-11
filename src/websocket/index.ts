8bff29882c902a23ba0dabe5bd86d5695951f50b
ting-xuan <a9523023650@gmail.com>
2023-05-11 14:28:50 +0800

    [ modify ] flag 改 once

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -5,36 +5,21 @@ import {
   type RawData,
 } from "ws";
 
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction, Application } from "express";
 
 export default class Socket extends WebSocketServer {
   static instance?: Socket;
-  static isRegister = false;
 
   static plugin(_req: Request, res: Response, next: NextFunction) {
-    if (!Socket.isRegister) {
-      Socket.isRegister = true;
-
-      const fn = res.app.emit;
-      res.app.emit = (eventName: string | symbol, ...args: any[]) => {
-        Socket.instance?.clients.forEach((value) => {
-          value.emit(eventName, ...args);
-        });
-        return fn.call(res.app, eventName, ...args);
-      };
-    }
-
+    Socket.instance?.emit("overrideAppMethod", res.app);
     return next();
   }
 
   static build(options: ServerOptions) {
-    Socket.instance = new Socket(
-      {
-        ...options,
-        path: "/socket",
-      },
-      () => console.log("[socket 開始]")
-    );
+    Socket.instance = new Socket({
+      ...options,
+      path: "/socket",
+    });
 
     Socket.instance.on("connection", (client) => {
       client.send(JSON.stringify({ type: "success" }));
@@ -45,11 +30,21 @@ export default class Socket extends WebSocketServer {
         }
 
         try {
-          this.handleClientMessage(client, data);
+          Socket.handleClientMessage(client, data);
         } catch (error) {
           client.send(JSON.stringify({ type: "error" }));
         }
       });
+    });
+
+    Socket.instance.once("overrideAppMethod", (app: Application) => {
+      const fn = app.emit;
+      app.emit = (eventName: string | symbol, ...args: any[]) => {
+        Socket.instance?.clients.forEach((value) => {
+          value.emit(eventName, ...args);
+        });
+        return fn.call(app, eventName, ...args);
+      };
     });
   }
 
