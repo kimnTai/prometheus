666d6a68cc1a43ba83469b87716d3a9daf8a2df7
ting-xuan <a9523023650@gmail.com>
2023-05-12 12:26:46 +0800

    [ modify ] 移除 models 靜態引用

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import morgan from "morgan";
 import "@/app/env";
 import "@/connection";
-import "@/models";
 import Routes from "@/routes";
 import * as Exception from "@/exception";
 import Socket from "@/websocket";
